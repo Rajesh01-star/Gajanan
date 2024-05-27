a347ed49f6364cd78d51081eb892a7f226fcac6e
Rajesh01-star <rajeshbehera489@gmail.com>
2024-05-27 16:05:14 +0530

    animations done

--- app/Components/PageHero.jsx
+++ app/Components/PageHero.jsx
@@ -1,4 +1,6 @@
+'use client'
 import React from "react";
+import { motion } from "framer-motion";
 
 const PageHero = ({ pageTitle }) => {
   return (
@@ -6,7 +8,19 @@ const PageHero = ({ pageTitle }) => {
       className="bg-cover bg-center py-20 text-white text-center"
       style={{ backgroundImage: "url('/pageHero.png')" }}
     >
-      <h1 className="text-4xl md:text-6xl font-bold mb-4">{pageTitle}</h1>
+      <motion.h1
+        initial={{ x: "-100vw" }}
+        animate={{ x: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 90,
+          damping: 20,
+          duration: 2,
+        }}
+        className="text-4xl md:text-6xl font-bold mb-4"
+      >
+        {pageTitle}
+      </motion.h1>
       <p className="text-lg md:text-xl">{`Explore our ${pageTitle} page`}</p>
     </div>
   );
